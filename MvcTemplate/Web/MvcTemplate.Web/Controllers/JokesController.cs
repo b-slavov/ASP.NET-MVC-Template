351e534b8fb3a4cc44da517a44091a0dbe1592ed
b-slavov <boyan.slavoff@gmail.com>
2016-02-24 13:36:06 +0200

    Added tests for IdentifierProvider!

---
+++ MvcTemplate/Web/MvcTemplate.Web/Controllers/JokesController.cs
@@ -0,0 +1,28 @@
+﻿namespace MvcTemplate.Web.Controllers
+{
+    using System.Web.Mvc;
+    using Infrastructure.Mapping;
+    using Services.Data;
+    using Services.Web;
+    using ViewModels.Home;
+
+    public class JokesController : BaseController
+    {
+        private IIdentifierProvider identifierProvider;
+        private IJokesService jokes;
+
+        public JokesController(IIdentifierProvider identifierProvider, IJokesService jokes)
+        {
+            this.identifierProvider = identifierProvider;
+            this.jokes = jokes;
+        }
+
+        public ActionResult ById(string id)
+        {
+            var joke = this.jokes.GetById(id);
+            var viewModel = AutoMapperConfig.Configuration.CreateMapper().Map<JokeViewModel>(joke);
+
+            return this.View(viewModel);
+        }
+    }
+}
