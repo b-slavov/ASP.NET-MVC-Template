1fe8a13dbaa5721393e12488013b27cdaf08320b
b-slavov <boyan.slavoff@gmail.com>
2016-02-24 14:09:44 +0200

    Route test added!

---
+++ MvcTemplate/Tests/MvcTemplate.Web.Routes.Tests/JokesRouteTests.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcTemplate.Web.Routes.Tests
+{
+    using System.Web.Routing;
+    using Controllers;
+    using MvcRouteTester;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JokesRouteTests
+    {
+        [Test]
+        public void TestRouteById()
+        {
+            const string url = "/Joke/NTIyNC4xMjMxMjMxMzEyMw==";
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            routeCollection.ShouldMap(url).To<JokesController>(c => c.ById("NTIyNC4xMjMxMjMxMzEyMw=="));
+        }
+    }
+}
